221a71cc7fd26909dc39ed015c4adfad1e07e7ef
乐之 <tianliang@yunjiglobal.com>
2020-11-26 02:23:25 +0800

    创建项目

---
+++ src/main/java/com/mvc/entity/User.java
@@ -0,0 +1,71 @@
+package com.mvc.entity;
+
+
+import java.util.Arrays;
+import java.util.Date;
+
+
+/**
+ * @author tl
+ * @date 2020/11/25
+ */
+
+
+public class User {
+    private Integer id;
+    private String name;
+    private Boolean gender;
+    private Date birth;
+    private String[] hobby;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", birth=" + birth +
+                ", hobby=" + Arrays.toString(hobby) +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String[] getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String[] hobby) {
+        this.hobby = hobby;
+    }
+}
---
+++ src/main/java/com/mvc/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.mvc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  // 声明后端控制器
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/test1")
+    public String hello1(){// service doGet doPost
+        System.out.println("hello1");
+        return "hello"; // /hello.jsp
+    }
+
+    @RequestMapping("/test2")
+    public String hello2(){// service doGet doPost
+        System.out.println("hello2");
+        return "hello2";
+    }
+}
